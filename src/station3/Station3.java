80b5071f1dbcc4521bdb94ccaf3eb076df1e91e0
mshr1-creat <19950786m@gmail.com>
2024-12-10 21:53:41 +0900

    Station3
    null を適切に扱えるようになろう

--- src/station3/Station3.java
+++ src/station3/Station3.java
@@ -7,11 +7,25 @@ public class Station3 {
 
     public static boolean checkEmailAddress(String email) {
         // 問題1: ここから
+    	
+    	if(email == null) {
+    		return false;
+    	}
+    	if(email.isEmpty()) {
+    		return false;
+    	}
+    	if(!email.contains("@")) {
+    		return false;
+    	}
+    	return true;
         // 問題1: ここまで
     }
 
     public static void q2(User user) {
         // 問題2: ここから
+    	if(user.name == null) {
+    		user.name = "未設定";
+    	}
         // 問題2: ここまで
     }
 }
