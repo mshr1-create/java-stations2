bea68e6ab9e1602c8034271b8f84631fe1d8a573
mshr1-creat <19950786m@gmail.com>
2024-12-10 21:54:25 +0900

    Station5
    例外を扱えるようになろう

--- src/station5/Station5.java
+++ src/station5/Station5.java
@@ -1,7 +1,12 @@
-public class Station5 {
+public class Station5  {
     public static void main(String[] args) {
         // ここから
-        process();
+    	try {
+    		process();
+    	}catch(RuntimeException e) {
+    		System.out.println("エラーが発生しました");
+    	}
+        
         // ここまで
         System.out.println("処理が完了しました");
     }
