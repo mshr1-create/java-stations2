7b2a1bf6f68de74e8af96be589a306ccd50232de
mshr1-creat <19950786m@gmail.com>
2024-12-10 21:53:14 +0900

    Station2
    ジェネリクスを扱えるようになろう

---
+++ src/station2/Cache.java
@@ -0,0 +1,15 @@
+
+public class Cache<K, V> {
+		
+    public V get(K key) {
+        return null;
+    }
+    
+    public void put(K key, V value) {
+        
+    }
+    
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ		
+    }
+}
