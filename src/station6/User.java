d66203e246ffca052381c24951b748815cb7b474
mshr1-creat <19950786m@gmail.com>
2024-12-10 21:54:44 +0900

    Station6
    Object 型と等価・等値について理解しよう

--- src/station6/User.java
+++ src/station6/User.java
@@ -1,5 +1,20 @@
+import java.util.Objects;
+
 public class User {
     int id;
     String name;
     String email;
+    
+    public boolean equals(Object other) {
+    	if(!(other instanceof User)) {
+    		return false;
+    	}
+    	User user = (User) other;
+    	return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email);
+    }
+    
+    public int hashCode() {
+    	return Objects.hash(id, name, email);
+    }
+    
 }
